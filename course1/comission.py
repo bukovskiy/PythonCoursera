fb74f820a7498724c0368649224e5d6350efb1a3
bukovskiy <vlad.uspenskiy@gmail.com>
2020-02-22 11:54:41 +0300

    one more task

--- course1/comission.py
+++ course1/comission.py
@@ -1,1 +1,23 @@
- .
+class Value():
+
+    def __init__(self):
+        self.amount = None
+
+    def __get__(self, obj, value):
+        return self.amount
+
+    def __set__(self, obj, value):
+        self.amount = int(value - value * obj.commission)
+
+
+
+class Account():
+    amount = Value()
+    def __init__(self, commission):
+        self.commission = commission
+
+# new_account = Account(0.1)
+# new_account.amount = 100
+#
+# print(new_account.amount)
+
